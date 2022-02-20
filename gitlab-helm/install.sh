8e3bb299de979b12dfa15500a15f40c6a9142d29
Dmitry P <dmitry@pereslegin.ru>
2022-02-20 04:46:07 +0300

    prometheus-operator

--- gitlab-helm/install.sh
+++ gitlab-helm/install.sh
@@ -3,5 +3,6 @@
 kubectl -n gitlab apply -f pv.yaml
 kubectl -n gitlab apply -f s3-backup.secret.yaml
 kubectl -n gitlab apply -f gitlab-keycloak-oidc.secret.yaml
+kubectl -n gitlab apply -f postgres.secret.yaml
 
-helm -n gitlab upgrade --install gitlab gitlab/gitlab -f values.production.yaml
+helm -n gitlab upgrade --install gitlab gitlab/gitlab -f values.yaml -f values.production.yaml
--- ingress-nginx/install.sh
+++ ingress-nginx/install.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 helm -n ingress-nginx install ingress-nginx nginx-stable/nginx-ingress -f values.yaml
+
+helm -n ingress-nginx upgrade --install ingress-nginx ingress-nginx/ingress-nginx -f values.yaml
